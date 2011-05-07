e7a9489965d00ccbd28b25e68abf78e9cff71466
Krikor Hovasapian <krikorh@unlv.nevada.edu>
2011-05-06 19:14:18 -0700

    Created tokenizer. Reads in header.txt file that mimics an HTTP header file (it's an actual header taken by Hostess).

--- Analyzer/main.cpp
+++ Analyzer/main.cpp
@@ -7,21 +7,56 @@
 //
 
 #include <iostream>
+#include <fstream>
 #include <string>
 #include "SearchTree.h"
 #include "SearchTree.cpp"
 
-using std::string;
+using namespace std;
 
 int main (int argc, const char * argv[])
 {
     
     BinarySearchTree<string> bst;
+    ifstream file;
+    string line;
+    char *cstr = NULL, *p = NULL;
     
     // insert code here...
-    std::cout << "HTTPParser v1.0\n";
+    cout << "HTTPParser v1.0" << endl;
     
-    bst.insert("Krikor");
+    // Open test file
+    file.open("../../../header.txt");
+    
+    if(file.is_open())
+    {
+        // Read in contents
+        while(file.good())
+        {
+            getline(file, line);
+            
+            //Grab tokens
+            cstr = new char [line.size()+1];
+            strncpy(cstr, line.c_str(), line.size()+1);
+            p = strtok(cstr, " ");
+            while(p != NULL)
+            {
+                cout << "Tokenizer: " << p << endl;
+                //Send each token to tree
+                bst.insert(p);
+                p = strtok(NULL, " ");
+            }
+            delete [] cstr;
+            //cout << line << endl;
+        }
+        
+        file.close();
+    }
+    else
+        cout << "Could not open file header.txt" << endl;
+    
+
+    /*bst.insert("Krikor");
     bst.insert("Bob");
     bst.insert("Susan");
     bst.insert("Tommy");
@@ -35,6 +70,8 @@ int main (int argc, const char * argv[])
     bst.insert("Kobe Bryant");
     bst.insert("3jlkjas09fljkjaasdf");
     bst.insert("bob");
+    bst.printTree();
+    */
     bst.printTree();
     
     return 0;
